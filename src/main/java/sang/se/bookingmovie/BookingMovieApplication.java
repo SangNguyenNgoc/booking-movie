91c5acd2c0634e7b14af82f1b5f645ba88166d54
SangNguyen <nngocsang38@gmail.com>
2023-09-13 13:04:02 +0700

    Initial commit

---
+++ src/main/java/sang/se/bookingmovie/BookingMovieApplication.java
@@ -0,0 +1,13 @@
+package sang.se.bookingmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sang/se/bookingmovie/BookingMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package sang.se.bookingmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
