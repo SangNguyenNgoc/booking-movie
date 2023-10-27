7f815b77efa921af81c5412ceb12236631cfcac8
SangNguyen <nngocsang38@gmail.com>
2023-10-27 10:50:01 +0700

    update 27/10 - deloy db

--- src/main/java/sang/se/bookingmovie/config/OpenApiConfig.java
+++ src/main/java/sang/se/bookingmovie/config/OpenApiConfig.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
                 contact = @Contact(
                         name = "Sang Nguyễn",
                         email = "nngocsang38@gmail.com",
-                        url = "https://github.com/SangNguyenNgoc"
+                        url = "https://github.com/SangNguyenNgoc/course-registration.git"
                 ),
                 description = "Open Api for booking movie app",
                 title = "OpenApi booking movie - Sang Nguyễn",
