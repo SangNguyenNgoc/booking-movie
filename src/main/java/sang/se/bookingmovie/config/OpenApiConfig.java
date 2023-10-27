8a736487fcc62402f93bfe36d9921ca3fb76ce5c
SangNguyen <nngocsang38@gmail.com>
2023-10-27 10:50:01 +0700

    update 27/10 - deloy db

--- src/main/java/sang/se/bookingmovie/config/OpenApiConfig.java
+++ src/main/java/sang/se/bookingmovie/config/OpenApiConfig.java
@@ -29,7 +29,7 @@ import org.springframework.context.annotation.Configuration;
         ),
         servers = {
                 @Server(
-                        description = "Heroku Application",
+                        description = "Localhost",
                         url = "https://booking-movie-backend-3a547b1ac2e9.herokuapp.com"
                 )
         }
